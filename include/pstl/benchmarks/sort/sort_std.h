57de6bf2e8e68441348082b13e548c463970df4b
Ruben Laso <ruben.laso.rodriguez@univie.ac.at>
2026-04-27 13:51:03 +0200

    other: use std::sort instead of tbb:sort (clarify memory usage warning)

--- include/pstl/benchmarks/sort/sort_std.h
+++ include/pstl/benchmarks/sort/sort_std.h
@@ -2,20 +2,11 @@
 
 #include <algorithm>
 
-#if defined(PSTL_BENCH_USE_TBB)
-#include <tbb/parallel_sort.h>
-#endif //USE_TBB
-
 namespace benchmark_sort
 {
 	const auto sort_std = [](auto && policy, auto & input) {
-#if defined(PSTL_BENCH_USE_TBB)
-	// Show compiler message if TBB is used
 #warning \
-    "Using tbb::parallel_sort since std::sort(parallel_policy) explodes in memory usage. See: https://github.com/llvm/llvm-project/issues/78956#issue-2093630075"
-		tbb::parallel_sort(input.begin(), input.end());
-#else
+    "Using std::sort(parallel_policy) may explode in memory usage. See: https://github.com/llvm/llvm-project/issues/78956#issue-2093630075"
 		std::sort(policy, input.begin(), input.end());
-#endif //USE_TBB
 	};
 } // namespace benchmark_sort
